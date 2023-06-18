a667e8a92d7924127089c82b7ee6fd53e4d6b59c
Hossam Alzamly <hossam.alzamly@operaize.com>
2023-06-18 02:47:45 +0200

    Edit deployment name

--- deployment/expose-external-ip.sh
+++ deployment/expose-external-ip.sh
@@ -1,3 +1,3 @@
-# Create a Service object that exposes the frontend deployment
+# Create a Service object that exposes the api-predict deployment
 # The command below will ceates an external load balancer and assigns a fixed, external IP to the Service.
 kubectl expose deployment api-predict --type=LoadBalancer --name=public-api
